8329eadac34849b42f046868b04bb355801c799b
Vladimir KAmbur <vladimir.kambur@googlemail.com>
2025-09-04 15:30:57 +0200

    Auto-generated commit

---
+++ chunk-CMGB2ZR5.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-GVPKON5T.js";import"./chunk-6YTPGPJU.js";import"./chunk-PRZ6JHFG.js";export{a as BookletCover};
--- chunk-O6AQCY7Z.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-7IOYEK6G.js";import"./chunk-6YTPGPJU.js";import"./chunk-PRZ6JHFG.js";export{a as BookletCover};
